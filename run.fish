f85bbf309c1ae9002d184c6b3631d3806c6f48e9
rubcuadra <rubcuadra@gmail.com>
2017-05-11 17:41:35 -0500

    added obj loader compile lines

--- run.fish
+++ run.fish
@@ -5,10 +5,11 @@
 #brew install sdl2_mixer
 #brew install boost
 
-#g++ *.cpp -framework OpenGL -framework GLUT -I/usr/local/include -L/usr/local/lib -lpng -lmysqlcppconn -lSDL2_mixer -lSDL2 -lboost_filesystem -lboost_system
-#./a.out
-gcc -DHAVE_CONFIG_H -g -O2 -c loader/*.c
-ar cru libglm.a loader/*.o
+cd loader
+gcc -DHAVE_CONFIG_H -g -O2 -c *.c
+ar cru libglm.a *.o
+mv libglm.a ..
+cd ..
 g++ -DHAVE_CONFIG_H -g -O2 -I/usr/local/include -c -std=c++11 *.cpp
 g++ *.o libglm.a -L/usr/local/lib -framework OpenGL -framework GLUT -ljpeg -lpng -lIL -lm -lmysqlcppconn  -lSDL2_mixer -lSDL2 -lboost_filesystem -lboost_system
 ./a.out
