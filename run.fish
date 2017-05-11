d6e3fcfa0e0881853cc8615c8baf2d90551419ab
rubcuadra <rubcuadra@gmail.com>
2017-05-11 17:44:57 -0500

    fixed remove o

--- run.fish
+++ run.fish
@@ -13,4 +13,4 @@ cd ..
 g++ -DHAVE_CONFIG_H -g -O2 -I/usr/local/include -c -std=c++11 *.cpp
 g++ *.o libglm.a -L/usr/local/lib -framework OpenGL -framework GLUT -ljpeg -lpng -lIL -lm -lmysqlcppconn  -lSDL2_mixer -lSDL2 -lboost_filesystem -lboost_system
 ./a.out
-rm *.o
+rm loader/*.o
